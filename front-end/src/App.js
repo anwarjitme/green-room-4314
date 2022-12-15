0027e53822a8d3bd50bafec3a5538e1c1d4fe03c
Vivek Anjan <vivekanjankc12345@gmail.com>
2022-12-15 20:17:59 +0530

    day-3

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Home_navbar from './Components/Navbar/Home_navbar';
 import AllRoute from "./Pages/AllRoute";
-
+//import Companydata from './Components/Company/Companydata';
+//import Company from './Pages/Company';
 function App() {
   return (
     <div className="App">
