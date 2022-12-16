5c03c2d74da5dc6c9552e3ae0db0fc915e0dee1d
18Kailash <sumer.ki359@gmail.com>
2022-12-16 22:08:12 +0530

    day-4-task-completed

--- front-end/src/Pages/AllRoute.jsx
+++ front-end/src/Pages/AllRoute.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Hnavbar from "./Hnavbar";
+
 import LoginPage from "./LoginPage";
 import SignupPage from "./SignupPage";
 
@@ -7,8 +9,11 @@ const AllRoute = () => {
   return (
     // <div>AllRoute</div>
     <Routes>
+       
+       <Route  path="/" element={<Hnavbar/>}></Route>
       <Route path="/signup" element={<SignupPage />}></Route>
       <Route path="/login" element={<LoginPage />}></Route>
+
     </Routes>
   );
 };
