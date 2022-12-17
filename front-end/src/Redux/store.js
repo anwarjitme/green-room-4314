a5f6312b5f453f5e5d68f154ece894867252a4ce
Vivek Anjan <vivekanjankc12345@gmail.com>
2022-12-17 22:25:25 +0530

    done

--- front-end/src/Redux/store.js
+++ front-end/src/Redux/store.js
@@ -1,13 +1,16 @@
 
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
-import { Contactreducer } from "./Contact/reducer";
+
+import {companyreducer} from "./Company/reducer"
+import {Contactreducer} from "./contact/reducer"
 import { userLoginReducer } from "./UserLogin/UserLogin.reducer";
 import { userSignupReducer } from "./UserSignup/userSignup.reducer";
 const rootReducer = combineReducers({
   signup: userSignupReducer,
   login: userLoginReducer,
- contact:Contactreducer
+ contact:Contactreducer,
+ companies:companyreducer,
 });
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
 
