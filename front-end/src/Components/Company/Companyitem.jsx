b676395470f8764e665e241d7d87c38ba8382ba7
Vivek Anjan <vivekanjankc12345@gmail.com>
2022-12-18 15:23:20 +0530

    done

--- front-end/src/Components/Company/Companyitem.jsx
+++ front-end/src/Components/Company/Companyitem.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
-
-const Companyitem = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import {Box,Tr,Td,Input, Heading,Checkbox} from '@chakra-ui/react'
+import { useDispatch } from 'react-redux'
+import { getdata,deletcompany } from '../../Redux/Company/action'
+import {DeleteIcon} from '@chakra-ui/icons'
+const Companyitem=({name,owner,createdata,phonenumber,lastdate,city,country,industry,id})=>{
+  const dispatch=useDispatch();
+  const deletkardo=(id)=>
+  {
+ //console.log(id)
+ dispatch(deletcompany(id)).then(()=>dispatch(getdata()))
+  }
+    return (
+        <>
+     
+        <Tr>
+           <Td> 
+            <Checkbox size='lg' >
+    
+  </Checkbox>
+  </Td> 
+        <Td border='1px solid grey'><Heading as='h2' size='sm'>{name}</Heading></Td>
+        <Td border='1px solid grey'><Heading as='h2' size='sm'>{owner}</Heading></Td>
+        <Td border='1px solid grey'>{createdata}</Td>
+        <Td border='1px solid grey'>{phonenumber}</Td>
+        <Td border='1px solid grey'>{lastdate}</Td>
+        <Td border='1px solid grey'>{city}</Td>
+        <Td border='1px solid grey'>{country}</Td>
+        <Td border='1px solid grey'>{industry}</Td>
+       
+        <Td border='1px solid grey' _hover={{color:'red'}} onClick={()=>deletkardo(id)}><DeleteIcon/></Td>
+      </Tr>
+        </>
+    )
 }
+export default Companyitem;
 
-export default Companyitem
